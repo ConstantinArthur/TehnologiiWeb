0baec82451a5a01e915a3d975444d1de39c22859
ConstantinArthur <arthur_stef.constantin@yahoo.com>
2025-10-21 17:07:47 +0300

    Delete Problema1 directory

--- Problema1/1.2.js
+++
@@ -1,9 +0,0 @@
-const birthYears = [2004, 2000, 1990, 2010, 1985, 2020, 2021];
-const currentYear = new Date().getFullYear();
-
-const ages = birthYears.map(year => currentYear - year);
-const adults = ages.filter(age => age >= 18);
-
-console.log("Ani de naștere:", birthYears);
-console.log("Vârste:", ages);
-console.log("Vârste peste 18 ani:", adults);
